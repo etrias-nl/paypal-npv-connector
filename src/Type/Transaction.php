763086ae68cda9ad6a0005dc28dd90adeae41324
Roland Franssen <franssen.roland@gmail.com>
2020-04-06 17:30:48 +0200

    mapping

--- src/Type/Transaction.php
+++ src/Type/Transaction.php
@@ -8,11 +8,33 @@ class Transaction
 {
     use QueryResultTrait;
 
+    public const STATUS_UNCLEARED = 'Uncleared';
+    public const STATUS_HELD = 'Held';
+    public const STATUS_REMOVED = 'Removed';
+    public const STATUS_REVERSED = 'Reversed';
+    public const STATUS_REFUNDED = 'Refunded';
+
     /** @var null|string */
     protected $status;
+
+    /** @var null|string */
+    protected $transactionId;
 
     public function getStatus(): ?string
     {
         return $this->status;
+    }
+
+    /**
+     * @param string|string[] $status
+     */
+    public function isStatus($status): bool
+    {
+        return $status === $this->status || (\is_array($status) && \in_array($this->status, $status, true));
+    }
+
+    public function getTransactionId(): ?string
+    {
+        return $this->transactionId;
     }
 }
